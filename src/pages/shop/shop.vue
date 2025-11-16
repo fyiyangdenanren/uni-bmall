670c7a308bc661f22676d410875452e870791b6a
马上星期五 <2869244577@qq.com>
2025-11-16 11:39:52 +0800

    feat:修复书库窗口尺寸

--- src/pages/shop/shop.vue
+++ src/pages/shop/shop.vue
@@ -4,13 +4,10 @@
     <common-search />
 
     <!-- 选择区域 -->
-    <view class="content">
-      <view class="empty"></view>
-      <view class="selection">
-        <text class="tab" v-for="(t, i) in tabs" :key="i" :class="{ active: currentTab === i }" @click="changeTab(i)">
-          {{ t }}
-        </text>
-      </view>
+    <view class="selection">
+      <text class="tab" v-for="(t, i) in tabs" :key="i" :class="{ active: currentTab === i }" @click="changeTab(i)">
+        {{ t }}
+      </text>
     </view>
 
     <!-- 分类列表区域 -->
@@ -37,7 +34,7 @@
 
 <script setup lang='ts'>
 import CommonSearch from '@/components/common-search.vue'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 // tab 集合
 const tabs = ['免费', '付费']
 // 当前选中索引
@@ -56,52 +53,50 @@ const changeActive = (index: number) => {
 const systemInfo = uni.getSystemInfoSync()
 // 获取当前屏幕的高度
 const wh = ref(systemInfo.windowHeight);
+onMounted(() => {
+  // 获取当前屏幕的高度
+  wh.value = systemInfo.windowHeight - uni.upx2px(200);
+})
 </script>
 
 <style scoped lang='scss'>
-.content {
+.selection {
+  height: 50rpx;
+  font-size: 32rpx;
+  font-weight: 400;
+  color: rgba(102, 102, 102, 1);
   display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-bottom: 20rpx;
+  margin-left: 200rpx;
 
-  .empty {
-    width: 200rpx;
-  }
+  .tab {
 
-  .selection {
-    width: 100%;
-    font-size: 32rpx;
-    font-weight: 400;
-    color: rgba(102, 102, 102, 1);
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    margin-bottom: 20rpx;
+    /* 激活项样式 */
+    &.active {
+      font-size: 40rpx;
+      font-weight: 600;
+      color: rgba(51, 51, 51, 1);
+      position: relative;
 
-    .tab {
-
-      /* 激活项样式 */
-      &.active {
-        font-size: 40rpx;
-        font-weight: 600;
-        color: rgba(51, 51, 51, 1);
-        position: relative;
-
-        /* 渲染激活底部的指示边线 */
-        &::before {
-          content: '';
-          position: absolute;
-          bottom: -10rpx; // 位于底部下方一点，可以根据需要调整
-          left: 50%;
-          transform: translateX(-50%);
-          width: 40rpx; // 宽度
-          height: 4rpx; // 高度
-          background: rgba(66, 134, 135, 1);
-          border-radius: 2rpx;
-        }
+      /* 渲染激活底部的指示边线 */
+      &::before {
+        content: '';
+        position: absolute;
+        bottom: -10rpx; // 位于底部下方一点，可以根据需要调整
+        left: 50%;
+        transform: translateX(-50%);
+        width: 40rpx; // 宽度
+        height: 4rpx; // 高度
+        background: rgba(66, 134, 135, 1);
+        border-radius: 2rpx;
       }
     }
-
   }
+
 }
+
 
 .categorylist {
   display: flex;

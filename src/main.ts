a80e15b44cf00f7c21a1b098a1a2ed77485dee24
马上星期五 <2869244577@qq.com>
2025-11-16 12:32:01 +0800

    feat:初始化pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createSSRApp } from "vue";
 import App from "./App.vue";
 import "@/common/base.scss"
+import pinia from "./stores/user";
 export function createApp() {
   const app = createSSRApp(App);
+  app.use(pinia);
   return {
     app,
   };
